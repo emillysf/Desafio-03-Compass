36391e22ba4a35c42e3f99e8d787dfad99a69eab
emillysfranca <emilly.sfrancaa@gmail.com>
2023-08-14 17:10:08 -0300

    fix(service): add methods post

--- src/main/java/br/com/pb/compass/challenge3/service/PostService.java
+++ src/main/java/br/com/pb/compass/challenge3/service/PostService.java
@@ -2,9 +2,21 @@ package br.com.pb.compass.challenge3.service;
 
 import br.com.pb.compass.challenge3.dto.PostDto;
 
+import java.util.List;
+
 
 public interface PostService {
-        PostDto processPost(Long postId);
+        void processPost(Long postId);
+        PostDto reprocessPost(Long postId);
+
+        PostDto getPostWithHistory(Long postId);
+        void togglePostStatus(Long postId);
+
+        List<PostDto> getAllPosts();
+        void processAllPosts();
+
+
+
 
 }
 

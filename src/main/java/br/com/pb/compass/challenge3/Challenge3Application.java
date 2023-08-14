d1f609355fc100ce37085377d8476f938da7288c
emillysfranca <emilly.sfrancaa@gmail.com>
2023-08-14 17:09:05 -0300

    fix: main

--- src/main/java/br/com/pb/compass/challenge3/Challenge3Application.java
+++ src/main/java/br/com/pb/compass/challenge3/Challenge3Application.java
@@ -1,10 +1,28 @@
 package br.com.pb.compass.challenge3;
 
+
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
+
 public class Challenge3Application {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Challenge3Application.class, args);

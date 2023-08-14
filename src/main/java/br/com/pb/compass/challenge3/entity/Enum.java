c3151881e707204bdb2940c0bc5fd38e0fd5d745
emillysfranca <emilly.sfrancaa@gmail.com>
2023-08-14 10:05:01 -0300

    feat(enum): add enum file

---
+++ src/main/java/br/com/pb/compass/challenge3/entity/Enum.java
@@ -0,0 +1,15 @@
+package br.com.pb.compass.challenge3.entity;
+
+public class Enum {
+    public enum PostState {
+        CREATED,
+        POST_FIND,
+        POST_OK,
+        COMMENTS_FIND,
+        COMMENTS_OK,
+        ENABLED,
+        DISABLED,
+        UPDATING,
+        FAILED
+    }
+}

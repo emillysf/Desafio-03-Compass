b782541620e06947e09e2ebe9cb297c376ba5601
emillysfranca <emilly.sfrancaa@gmail.com>
2023-08-14 09:49:57 -0300

    feat(DTO): add comment DTO file

---
+++ src/main/java/br/com/pb/compass/challenge3/dto/CommentDto.java
@@ -0,0 +1,13 @@
+package br.com.pb.compass.challenge3.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentDto {
+    private Long id;
+    private String body;
+}

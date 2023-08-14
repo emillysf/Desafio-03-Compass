e7782091e5e826bf49468a6f95ae41d41dcc5cb5
emillysfranca <emilly.sfrancaa@gmail.com>
2023-08-14 09:51:11 -0300

    feat(service): add post service file

---
+++ src/main/java/br/com/pb/compass/challenge3/service/PostService.java
@@ -0,0 +1,10 @@
+package br.com.pb.compass.challenge3.service;
+
+import br.com.pb.compass.challenge3.dto.PostDto;
+
+
+public interface PostService {
+        PostDto processPost(Long postId);
+
+}
+

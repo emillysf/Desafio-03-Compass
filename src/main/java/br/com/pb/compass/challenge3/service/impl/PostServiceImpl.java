436223486e7e5d3c3ebd1b05bda4a36dadee2383
emillysfranca <emilly.sfrancaa@gmail.com>
2023-08-14 17:10:47 -0300

    fix(service): add methods postIMpl

--- src/main/java/br/com/pb/compass/challenge3/service/impl/PostServiceImpl.java
+++ src/main/java/br/com/pb/compass/challenge3/service/impl/PostServiceImpl.java
@@ -8,61 +8,73 @@ import br.com.pb.compass.challenge3.entity.Comment;
 import br.com.pb.compass.challenge3.entity.Enum;
 import br.com.pb.compass.challenge3.entity.History;
 import br.com.pb.compass.challenge3.entity.Post;
+import br.com.pb.compass.challenge3.exceptions.PostNotFoundException;
 import br.com.pb.compass.challenge3.repository.PostRepository;
-import br.com.pb.compass.challenge3.service.MessageSenderService;
 import br.com.pb.compass.challenge3.service.PostService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class PostServiceImpl implements PostService {
     private final ClientExternal clientExternal;
     private final PostRepository repository;
-    private final MessageSenderService message;
     private final HistoryServiceImpl historyServiceImpl;
-    private ModelMapper mapper;
+    private final ModelMapper mapper;
+//    private final JmsTemplate jmsTemplate;
+//    private final String jmsQueue;
 
     @Autowired
-    public PostServiceImpl(ClientExternal clientExternal, PostRepository repository, ModelMapper mapper, HistoryServiceImpl historyServiceImpl, MessageSenderService message) {
+    public PostServiceImpl(ClientExternal clientExternal, PostRepository repository, ModelMapper mapper,
+                           HistoryServiceImpl historyServiceImpl) {
         this.clientExternal = clientExternal;
         this.repository = repository;
         this.mapper = mapper;
         this.historyServiceImpl = historyServiceImpl;
-        this.message = message;
+//        this.jmsTemplate = jmsTemplate;
+//        this.jmsQueue = jmsQueue;
     }
 
+    @Async("asyncExecutor")
     @Override
-    public PostDto processPost(Long postId) {
+    public void processPost(Long postId) {
         Optional<Post> optionalPost = repository.findById(postId);
-        Post post;
+        Post post = optionalPost.orElseGet(() -> createNewPost(postId));
 
-        if (optionalPost.isPresent()) {
-            post = optionalPost.get();
-        } else {
-            post = new Post();
-            post.setId(postId);
-            message.sendPostProcessedMessage(postId);
-            historyServiceImpl.updateHistory(post, Enum.PostState.CREATED);
-        }
+        postExternalData(postId, post);
+        getComments(post);
+        PostDto postDto = convertToDto(post);
+        updatePostHistoryAndSave(post, postDto);
+//        jmsTemplate.convertAndSend(jmsQueue, postId.toString());
+    }
 
+    private Post createNewPost(Long postId) {
+        Post post = new Post();
+        post.setId(postId);
+        historyServiceImpl.updateHistory(post, Enum.PostState.CREATED);
+        return post;
+    }
+
+    private void postExternalData(Long postId, Post post) {
         historyServiceImpl.updateHistory(post, Enum.PostState.POST_FIND);
-
         PostDto postDto = clientExternal.getExternalPost(postId);
 
-        if (postDto == null) {
-            return convertToDTO(post);
+        if (postDto != null) {
+            post.setTitle(postDto.getTitle());
+            post.setBody(postDto.getBody());
+            historyServiceImpl.updateHistory(post, Enum.PostState.POST_OK);
         }
+    }
 
-        post.setTitle(postDto.getTitle());
-        post.setBody(postDto.getBody());
-        historyServiceImpl.updateHistory(post, Enum.PostState.POST_OK);
+    private void getComments(Post post) {
+        List<CommentDto> commentsDto = clientExternal.getExternalCommentsForPost(post.getId());
 
-        List<CommentDto> commentsDto = clientExternal.getExternalCommentsForPost(postId);
         if (commentsDto != null && !commentsDto.isEmpty()) {
             historyServiceImpl.updateHistory(post, Enum.PostState.COMMENTS_FIND);
 
@@ -78,43 +90,143 @@ public class PostServiceImpl implements PostService {
             post.setComments(comments);
             historyServiceImpl.updateHistory(post, Enum.PostState.COMMENTS_OK);
         }
+    }
 
-        List<Comment> postComments = post.getComments();
-        List<CommentDto> commentDtos = new ArrayList<>();
-        for (Comment comment : postComments) {
-            CommentDto commentDto = new CommentDto();
-            commentDto.setId(comment.getId());
-            commentDto.setBody(comment.getBody());
-            commentDtos.add(commentDto);
-        }
+    private PostDto convertToDto(Post post) {
+        PostDto postDto = mapper.map(post, PostDto.class);
+        List<CommentDto> commentDtos = post.getComments().stream()
+                .map(this::convertCommentToDto)
+                .collect(Collectors.toList());
         postDto.setComments(commentDtos);
+        return postDto;
+    }
 
+    private CommentDto convertCommentToDto(Comment comment) {
+        CommentDto commentDto = new CommentDto();
+        commentDto.setId(comment.getId());
+        commentDto.setBody(comment.getBody());
+        return commentDto;
+    }
 
+    private void updatePostHistoryAndSave(Post post, PostDto postDto) {
         List<History> postHistory = post.getHistory();
-        List<HistoryDto> historyDtos = new ArrayList<>();
-        for (History history : postHistory) {
-            historyDtos.add(historyServiceImpl.convertToDto(history));
-        }
+        List<HistoryDto> historyDtos = postHistory.stream()
+                .map(historyServiceImpl::convertToDto)
+                .collect(Collectors.toList());
         postDto.setHistory(historyDtos);
 
         repository.save(post);
         historyServiceImpl.updateHistory(post, Enum.PostState.ENABLED);
-        historyServiceImpl.updateHistory(post, Enum.PostState.DISABLED);
-        historyServiceImpl.updateHistory(post, Enum.PostState.UPDATING);
-
-        return postDto;
     }
 
 
+    @Override
+    public PostDto reprocessPost(Long postId) {
+        Optional<Post> optionalPost = repository.findById(postId);
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
 
-    private PostDto convertToDTO(Post post) {
-        PostDto postDto = mapper.map(post, PostDto.class);
-        return postDto;
+            historyServiceImpl.updateHistory(post, Enum.PostState.UPDATING);
+            postExternalData(postId, post);
+            getComments(post);
+
+            updatePostFieldsWithExternalData(postId, post);
+
+            PostDto postDto = convertToDto(post);
+            repository.save(post);
+
+            historyServiceImpl.updateHistory(post, Enum.PostState.ENABLED);
+
+            return postDto;
+        } else {
+            try {
+                throw new PostNotFoundException("Post with ID " + postId + " not found");
+            } catch (PostNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
-    private Post mapToEntity(PostDto postDto){
-        Post post = mapper.map(postDto, Post.class);
-        return post;
+    private void updatePostFieldsWithExternalData(Long postId, Post post) {
+        PostDto postDto = clientExternal.getExternalPost(postId);
+
+        if (postDto != null) {
+            post.setTitle(postDto.getTitle());
+            post.setBody(postDto.getBody());
+        }
+        List<History> postHistory = post.getHistory();
+        List<HistoryDto> historyDtos = postHistory.stream()
+                .map(historyServiceImpl::convertToDto)
+                .collect(Collectors.toList());
+        postDto.setHistory(historyDtos);
     }
+
+    @Override
+    public void togglePostStatus(Long postId) {
+        Optional<Post> optionalPost = repository.findById(postId);
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+            Enum.PostState currentState = determineCurrentState(post);
+
+            if (currentState == Enum.PostState.DISABLED) {
+                historyServiceImpl.updateHistory(post, Enum.PostState.ENABLED);
+            } else {
+                historyServiceImpl.updateHistory(post, Enum.PostState.DISABLED);
+            }
+
+            repository.save(post);
+        } else {
+            try {
+                throw new PostNotFoundException("Post with ID " + postId + " not found");
+            } catch (PostNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private Enum.PostState determineCurrentState(Post post) {
+        List<History> history = post.getHistory();
+        if (history != null && !history.isEmpty()) {
+            Enum.PostState lastState = history.get(history.size() - 1).getState();
+            if (lastState == Enum.PostState.DISABLED) {
+                return Enum.PostState.DISABLED;
+            }
+        }
+        return Enum.PostState.ENABLED;
+    }
+
+    @Override
+    public PostDto getPostWithHistory(Long postId) {
+        Optional<Post> optionalPost = repository.findById(postId);
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+            PostDto postDto = convertToDto(post);
+            return postDto;
+        } else {
+            try {
+                throw new PostNotFoundException("Post with ID " + postId + " not found");
+            } catch (PostNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public List<PostDto> getAllPosts() {
+        List<Post> posts = repository.findAll();
+        return posts.stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void processAllPosts() {
+        List<Post> posts = repository.findAll();
+        for (Post post : posts) {
+            processPost(post.getId());
+        }
+    }
+
+
 
 }

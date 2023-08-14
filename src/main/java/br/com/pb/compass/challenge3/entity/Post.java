44e0a8d946f6af6639a1e63962dfba55f2e6bf4d
emillysfranca <emilly.sfrancaa@gmail.com>
2023-08-14 09:47:31 -0300

    feat(entity): add post entity file

---
+++ src/main/java/br/com/pb/compass/challenge3/entity/Post.java
@@ -0,0 +1,32 @@
+package br.com.pb.compass.challenge3.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String body;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<History> history = new ArrayList<>();
+}

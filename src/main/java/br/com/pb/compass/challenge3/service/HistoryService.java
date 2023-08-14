792132a22192fb9461eadfd860411157615fdeb5
emillysfranca <emilly.sfrancaa@gmail.com>
2023-08-14 09:51:30 -0300

    feat(service): add history service file

---
+++ src/main/java/br/com/pb/compass/challenge3/service/HistoryService.java
@@ -0,0 +1,13 @@
+package br.com.pb.compass.challenge3.service;
+
+import br.com.pb.compass.challenge3.dto.HistoryDto;
+import br.com.pb.compass.challenge3.entity.Enum;
+import br.com.pb.compass.challenge3.entity.History;
+import br.com.pb.compass.challenge3.entity.Post;
+
+public interface HistoryService {
+
+    void updateHistory(Post post, Enum.PostState newState);
+    HistoryDto convertToDto(History history);
+    History convertToEntity(HistoryDto historyDto);
+}
